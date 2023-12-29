ecb8304580bc906804a3c3f4f5ae8a3b826cf661
ghizlane <ghlaneiz022@gmail.com>
2023-12-29 15:19:39 +0100

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+	var skills = ["HTML", "CSS", "JS", "Python"];
+	var member = {
+		name: "Shahriar",
+		age: 26,
+		skills: skills,
+	};
+
+	console.log(member.skills[2]);
+}
